4e493f7eec8c72ae9bd2e8a30472a6d35d8c2d53
Gangisetty-falki <gangisettyfalki@gmail.com>
2024-11-19 20:44:34 +0530

    Create count_notebooks.py

---
+++ visaa_prep/count_notebooks.py
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner f=new Scanner(System.in);
+        int n=f.nextInt();
+        System.out.println(n*10);
+    }
+}
